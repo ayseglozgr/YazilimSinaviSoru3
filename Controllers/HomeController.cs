5fa5539935e8359d2c68a08ec1ab16fbb1568649
ayseglozgr <a.ozgurr0@gmail.com>
2023-09-04 15:24:31 +0300

    Add project files.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace YazilimSinaviSoru3.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Post(string ad, string soyad, string tarih, int yas, string sehir)
+        {
+            
+            ViewBag.Mesaj = "Bilgileriniz başarıyla gönderildi.";
+
+            return View("Contact");
+        }
+
+    }
+}
---
+++ Models/ContactModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace YazilimSinaviSoru3.Models
+{
+    public class ContactModel
+    {
+        public string Ad { get; set; }
+        public string Soyad { get; set; }
+
+        public DateTime Tarih { get; set; }
+        public int Yas { get; set; }
+        public string Sehir { get; set; }
+    }
+}
